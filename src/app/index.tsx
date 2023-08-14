e25fc476b94c980e96fa4fcfa7ef5591ba7a5ba1
Kogay Artem <kogay10987@gmail.com>
2023-08-14 20:17:50 +0500

    vercel 1.4

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -39,7 +39,7 @@ const App = () => {
                 <AboutMe />
                 <Skills />
                 <Project />
-                <a href="https://resume-amber-theta.vercel.app/src/app/assets/resume.pdf" download="resume.pdf">скачать</a>
+                <a href="/assets/resume.pdf" download="resume.pdf">скачать</a>
                 <Footer />
             </Container>
         </AppStyle>
